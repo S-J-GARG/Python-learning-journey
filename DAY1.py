c4b62362f839279811982905093194d191a9cf0f
SHIVANK GARG <117145235+S-J-GARG@users.noreply.github.com>
2022-11-25 15:37:10 +0530

    Add files via upload

---
+++ DAY1.py
@@ -0,0 +1,44 @@
+print(3/4)
+print(3%4)
+print(3//4)
+
+a=10;b=25;c=15;d=30;e=2;f=3;g=5
+w=a+b-c; x=d**e; y=f%g
+print(w,x,y)
+#use of float
+i=3.5
+j=1.2
+print(i%j)
+#use of complex
+a1=1+2j
+b1=3*a1
+c1=a1*b1
+print(a1)
+print(b1)
+print(c1)
+print(a1.real)
+print(b1.real)
+print(a1.imag)
+print(a1.conjugate())
+print(b1.conjugate())
+#use of boolen
+x=True
+y=3>4
+print(x)
+print(y)
+#convert to int
+print(int(3.12))
+a=int('485')
+b=int('786')
+c=a+b
+print(c)
+print(int('1011',2))
+print(int('1111',2))
+print(int('341',8))
+print(int('21',16))
+
+
+
+
+
+
